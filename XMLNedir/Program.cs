bda2442fa0ac9be583a2a281a155582521635e6b
Alp Sarıkışla <50963484+alpsarikisla@users.noreply.github.com>
2022-10-30 12:52:45 +0300

    Dosya Oluşturma Okuma Tamamlandı

---
+++ XMLNedir/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XMLNedir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tanımlama
+
+            //Türkçe açılımı Genişletilebilir işaretleme dili anlamına gelen XML(Extensible Markup Language), bilgi işlem sistemleri için kolayca okunabilecek veri dökümenları yani veri tabanı oluşturmaya yarayan, W3C tarafından tanımlanmış bir standarttır. 
+            //Veri saklama görevinin yanısıra daha çok kullanılan alanı farklı sistemlerde(Farklı platformlar ve farklı diller) oluşturulmuş programlar arasında veri alışverişi konusunda entegrasyon görevi üstlenir.
+
+            #endregion
+
+            XmlIslemleri xi = new XmlIslemleri("Personeller.xml");
+
+            #region XML Dosyası Oluşturma
+
+            //xi.Olustur();
+
+            #endregion
+
+            #region XML Okuma
+
+            //xi.Oku();
+
+            #endregion
+
+            #region XML Ekleme
+
+            Console.Write("ID = ");
+            string id = Console.ReadLine();
+            Console.Write("İsim = ");
+            string isim = Console.ReadLine();
+            Console.Write("Soyad = ");
+            string soyad = Console.ReadLine();
+            Console.Write("Telefon = ");
+            string telefon = Console.ReadLine();
+
+            xi.Ekle(id, isim, soyad, telefon);
+            xi.Oku();
+
+            #endregion
+        }
+    }
+}
---
+++ XMLNedir/XmlIslemleri.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XMLNedir
+{
+    public class XmlIslemleri
+    {
+        string FilePath;
+        public XmlIslemleri(string path)
+        {
+            FilePath = path;
+        }
+        public void Olustur()
+        {
+            StreamWriter fs = new StreamWriter(FilePath);
+            //string Initial = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"+Environment.NewLine + "<Personeller>" + Environment.NewLine + "</personeller>";
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            sb.AppendLine("<Personeller>");
+            sb.AppendLine("</Personeller>");
+            fs.WriteLine(sb.ToString());
+            fs.Close();
+        }
+
+        public void Oku()
+        {
+            XDocument doc = XDocument.Load(FilePath);
+            XElement root = doc.Root;
+            string ID, isim, soyad, telefon;
+            foreach (XElement item in root.Elements())
+            {
+                ID = item.Attribute("id").Value;
+                isim = item.Element("isim").Value;
+                soyad = item.Element("soyisim").Value;
+                telefon = item.Element("telefon").Value;
+                string telefoncode = telefon.Substring(0, 7) + "****";
+                Console.WriteLine($"ID= {ID}\nIsim={isim}\nSoyad={soyad}\nTelefon={telefoncode}");
+            }
+        }
+
+        public void Ekle(string id, string isim, string soyad, string telefon)
+        {
+            XDocument doc = XDocument.Load(FilePath);
+            XElement root = doc.Root;
+            XElement yeni = new XElement("Personel");
+            XAttribute idAttribute = new XAttribute("id", id);
+            XElement isimelement = new XElement("isim", isim);
+            XElement soyisimelement = new XElement("soyisim", soyad);
+            XElement telefonelement = new XElement("telefon", telefon);
+            yeni.Add(idAttribute, isimelement, soyisimelement, telefonelement);
+            root.Add(yeni);
+            doc.Save(FilePath);
+        }
+    }
+}
